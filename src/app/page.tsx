abb0acb9a5cb377b73df8faff4d5603910947e70
YourMori <pa4ok_28@mail.ru>
2024-05-15 05:22:27 +0700

    Созданы секции CourseOverview, MainOverview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
+import Image from 'next/image'
+
+import CourseOverview from '@/widgets/CourseOverview'
 import { Button } from '@/shared/Button'
 import TeamCard from '@/shared/TeamCard'
 import ToggleThemeButton from '@/shared/ToggleThemeButton'
 import { Question } from '@/shared/UI/Question'
 import SearchCourse from '@/shared/UI/SearchCourse'
-import Image from 'next/image'
+import MainOverview from '@/widgets/MainOverview'
 
 export default function Home() {
   return (
-    <main className="flex h-screen w-screen flex-col items-center justify-center gap-[20px] ">
-      <ToggleThemeButton />
-      <Question question="weweaefqwfqefqwff" answer="cqewfwefwef" />
-      <SearchCourse />
+    <main className="min-h-screen w-screen">
+      <CourseOverview />
+      <MainOverview />
     </main>
   )
 }
