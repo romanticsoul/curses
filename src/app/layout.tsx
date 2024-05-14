73344824ad4561ee5d89c1b88cbd9631578644d4
julia_yarlykova <115877136+JuliaYarlykova@users.noreply.github.com>
2024-05-14 19:22:23 +0700

    Удалена лишняя строка

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import { Inter as FontSans } from 'next/font/google'
 
 import { ThemeProvider } from 'next-themes'
-import { cn } from '@/lib/utils'
+
 
 import './globals.css'
 
