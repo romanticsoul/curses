35a41a7909acc5b8d981e443ae2918b18480f69a
JuliaYarlykova <yarlykova2506@gmail.com>
2024-05-15 15:52:04 +0700

    добавила кнопки в карточки

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import Image from 'next/image'
-
-import CourseOverview from '@/widgets/CourseOverview'
-import { Button } from '@/shared/Button'
-import TeamCard from '@/shared/TeamCard'
 import ToggleThemeButton from '@/shared/ToggleThemeButton'
-import { Question } from '@/shared/UI/Question'
-import SearchCourse from '@/shared/UI/SearchCourse'
+import CourseOverview from '@/widgets/CourseOverview'
 import MainOverview from '@/widgets/MainOverview'
 
 export default function Home() {
