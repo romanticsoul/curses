6873ab6c4f6299717c13d0748fafa9d6d3d69455
Ainur Yarulin <yarulinainur@gmail.com>
2024-05-16 20:48:45 +0700

    create checkbox component

--- src/widgets/SwiperBlock.tsx
+++ src/widgets/SwiperBlock.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/shared/Button'
 import 'swiper/css'
 import { A11y, Navigation, Pagination, Scrollbar } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { cn } from '@/lib/utils'
 
 type Teacher = {
   id: number
@@ -36,10 +37,12 @@ const SwiperBlock = () => {
       <h2 className="mb-[48px] text-center text-[64px] font-medium text-benefit max-[1150px]:text-[70px] max-[767px]:text-[28px] max-[767px]:leading-7">
         Преподаватели
       </h2>
-      <div className={teachers.length < 3 ? 'hidden' : 'max-[767px]:hidden'}>
+      <div
+        className={cn(teachers.length < 3 ? 'hidden' : 'max-[767px]:hidden')}
+      >
         <Swiper
           modules={[Navigation, Pagination, Scrollbar, A11y]}
-          initialSlide={2}
+          initialSlide={0}
           spaceBetween={20}
           slidesPerView={3}
           // centeredSlides={true}
@@ -53,6 +56,7 @@ const SwiperBlock = () => {
             nextEl: nextButtonRef.current,
           }}
           onInit={() => setInit(true)}
+          className="container"
         >
           {teachers.map((d, key) => (
             <SwiperSlide key={key} className="overflow-hidden">
@@ -64,23 +68,23 @@ const SwiperBlock = () => {
               />
             </SwiperSlide>
           ))}
+          <Button
+            ref={prevButtonRef}
+            aspect={'square'}
+            className="absolute left-5 top-1/2 z-20 border-none bg-secondary text-primary hover:bg-secondary dark:bg-primary dark:text-foreground"
+          >
+            <span className="material-symbols-outlined rotate-180">
+              arrow_forward_ios
+            </span>
+          </Button>
+          <Button
+            ref={nextButtonRef}
+            aspect={'square'}
+            className="absolute right-5 top-1/2  z-20 border-none bg-secondary text-primary hover:bg-secondary dark:bg-primary dark:text-foreground"
+          >
+            <span className="material-symbols-outlined">arrow_forward_ios</span>
+          </Button>
         </Swiper>
-        <Button
-          ref={prevButtonRef}
-          aspect={'square'}
-          className="absolute left-[207px] top-1/2 z-20 border-none bg-secondary text-primary hover:bg-secondary dark:bg-primary dark:text-foreground max-[1219px]:left-[125px]"
-        >
-          <span className="material-symbols-outlined rotate-180">
-            arrow_forward_ios
-          </span>
-        </Button>
-        <Button
-          ref={nextButtonRef}
-          aspect={'square'}
-          className="absolute right-[207px] top-1/2 z-20 border-none bg-secondary text-primary hover:bg-secondary dark:bg-primary dark:text-foreground max-[1219px]:right-[125px]"
-        >
-          <span className="material-symbols-outlined">arrow_forward_ios</span>
-        </Button>
       </div>
       <ul
         className={
