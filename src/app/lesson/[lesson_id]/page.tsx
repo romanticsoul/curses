6a8444561c5405ce72972fd871a6d21ed9bf8820
JuliaYarlykova <yarlykova2506@gmail.com>
2024-05-17 18:05:55 +0700

    немного адаптива

--- src/app/lesson/[lesson_id]/page.tsx
+++ src/app/lesson/[lesson_id]/page.tsx
@@ -8,9 +8,9 @@ const Lesson = ({
   params: { lesson_id: string }
 }) => {
   return (
-    <main className="mb-[125px] flex min-h-screen w-screen ">
+    <main className="mb-[125px] mt-[36px] flex min-h-screen w-screen max-[880px]:flex-col-reverse max-[880px]:items-center">
       <LessonsPanel />
-      <div className="max-w-[70%]  px-[60px]">
+      <div className="max-w-[70%]  px-[60px] max-[880px]:mb-[20px] max-[880px]:max-w-[100%] max-[768px]:px-[20px]">
         <LessonTitle id={lesson_id} />
         <LessonMain />
         <Answer />
--- src/widgets/Lesson/LessonTitle.tsx
+++ src/widgets/Lesson/LessonTitle.tsx
@@ -10,18 +10,18 @@ const LessonTitle: React.FC<ILesson> = ({ id }) => {
       <div className="flex flex-row ">
         <Button
           aspect={'square'}
-          className=" border-none bg-secondary text-primary hover:bg-secondary dark:bg-primary dark:text-foreground"
+          className="border-none bg-secondary text-primary hover:bg-secondary dark:bg-primary dark:text-foreground  "
         >
           <span className="material-symbols-outlined rotate-180">
             arrow_forward_ios
           </span>
         </Button>
-        <h1 className="pl-[40px] text-[32px] font-normal">
+        <h1 className=" px-[40px] text-[32px]  font-normal max-[1049px]:px-[20px] max-[1049px]:text-[24px] max-[880px]:text-[20px]">
           Задание 1. Очень длинное название задания аж на 2 строчки
         </h1>
         <Button
           aspect={'square'}
-          className=" border-none bg-secondary text-primary hover:bg-secondary dark:bg-primary dark:text-foreground"
+          className="border-none bg-secondary text-primary hover:bg-secondary dark:bg-primary dark:text-foreground "
         >
           <span className="material-symbols-outlined">arrow_forward_ios</span>
         </Button>
