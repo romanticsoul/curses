9a1178dc0e26510721d3cda9ef53a80d2cc5ac96
JuliaYarlykova <yarlykova2506@gmail.com>
2024-05-18 19:03:55 +0700

    Добавлено изменение текста по нажатию на кнопку

--- src/shared/UI/CourseCard.tsx
+++ src/shared/UI/CourseCard.tsx
@@ -12,6 +12,7 @@ interface ICourseCard {
 
 const CourseCard: React.FC<ICourseCard> = ({ id, title, description }) => {
   const [ButtonProp, setButtonProp] = useState('Выбрать')
+  const [LikeActive, setActive] = useState(false)
 
   return (
     <li className="relative h-[287px] max-w-[287px] list-none rounded-2xl border-2 border-secondary bg-card duration-500 before:absolute before:block before:size-full before:bg-[url('/background/cardLine.png')] before:bg-center before:bg-no-repeat hover:bg-card-hover">
@@ -25,11 +26,14 @@ const CourseCard: React.FC<ICourseCard> = ({ id, title, description }) => {
           </p>
           <div className=" flex flex-row gap-[16px] ">
             <Button
-              variant={'outlined'}
-              className="min-w-0 shrink grow bg-card "
+              variant={ButtonProp === 'Выбрать' ? 'outlined' : 'primary'}
+              className={' min-w-0 shrink grow '}
               onClick={(e) => {
                 e.preventDefault()
                 e.stopPropagation()
+                setButtonProp(
+                  ButtonProp === 'Выбрать' ? 'Вы участник' : 'Выбрать'
+                )
               }}
             >
               {ButtonProp}
@@ -37,13 +41,18 @@ const CourseCard: React.FC<ICourseCard> = ({ id, title, description }) => {
             <Button
               aspect={'square'}
               variant={'outlined'}
-              className="shrink-0  border-secondary bg-white dark:border-primary"
+              className="shrink-0  border-secondary  dark:border-primary"
               onClick={(e) => {
                 e.preventDefault()
                 e.stopPropagation()
+                setActive(!LikeActive)
               }}
             >
-              <span className="material-symbols-outlined text-[#2C41FF]">
+              <span
+                className={
+                  'material-symbols-outlined fill-[#2C41FF] text-[#2C41FF]'
+                }
+              >
                 favorite
               </span>
             </Button>
